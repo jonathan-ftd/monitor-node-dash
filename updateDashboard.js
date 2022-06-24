e6a8083c8a1b78a46070c3b4cb8aade0b574a320
Johnatan <johnatan.penaloza@farmatodo.com>
2022-06-23 19:42:08 -0500

    update dashboard

---
+++ updateDashboard.js
@@ -0,0 +1,68 @@
+const axios = require("axios");
+
+const ENDPOINT = "https://monitor30-dashboard-dot-stunning-base-164402.uc.r.appspot.com/backend/flexible/v2/monitor/updateDashboard30";
+const DASHBOARDS = [
+    {
+        name: "EXPRESS", 
+        deltaTime: 30000
+    },
+    {
+        name: "FLASH", 
+        deltaTime: 60000
+    },
+    {
+        name: "FRAUD", 
+        deltaTime: 180000
+    },
+    {
+        name: "FACTURADAS", 
+        deltaTime: 180000
+    },
+    {
+        name: "SPECIALS", 
+        deltaTime: 60000
+    },
+    {
+        name: "RX", 
+        deltaTime: 180000
+    },
+    {
+        name: "PROGRAMMED", 
+        deltaTime: 120000
+    },
+    {
+        name: "MESSENGERS", 
+        deltaTime: 180000
+    },
+    {
+        name: "CANCELED", 
+        deltaTime: 280000
+    },
+    {
+        name: "PAYU_WITHOUT_FINALIZE", 
+        deltaTime: 180000
+    }
+];
+const TIMEZONE = {timeZone: "America/Bogota"};
+//const INTERVAL_TIME = 30000;
+
+(function() {
+    const updateDashboardMonitor = async function(dashboardType) {
+        try {
+            const res = await axios.get(`${ENDPOINT}/${dashboardType}`);
+            console.log(dashboardType, new Date().toLocaleString("en-US", TIMEZONE), res.data.data.message);
+        } catch (err) {
+            console.log(new Date().toLocaleString("en-US", TIMEZONE), "ERROR WHEN CALLING updateDashboard30", dashboardType, err)
+        }
+    } 
+
+    try {
+        for (let dash of DASHBOARDS) {
+            setInterval(() => {
+                updateDashboardMonitor(dash.name);
+             }, dash.deltaTime);
+        }
+    } catch (err) {
+        console.log("ERROR WHEN CALLING INTERVAL", err);
+    }
+})();
